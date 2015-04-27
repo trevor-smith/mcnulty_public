a152fc957d4a3a7dc1601b66ba9c046ab4338858
trevor smith <trevor.stuard@gmail.com>
2015-04-27 15:34:02 -0400

    plugging away

--- challenges.py
+++ challenges.py
@@ -1,18 +1,20 @@
-
+## Challenge 1
+# read in data
 import pandas as pd
 import numpy as np
 
-headers_all = ['party', 'handicapped-infants', 'water-project-cost-sharing', 
-'adoption-of-the-budget-resolution', 'physician-fee-freeze', 
-'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban', 
-'aid-to-nicraguan-contras', 'mx-missile', 'immigration', 
-'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue', 
+headers_all = ['party', 'handicapped-infants', 'water-project-cost-sharing',
+'adoption-of-the-budget-resolution', 'physician-fee-freeze',
+'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban',
+'aid-to-nicraguan-contras', 'mx-missile', 'immigration',
+'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue',
 'crime', 'duty-free-exports', 'export-administration-act-south-africa']
 df = pd.read_csv('data.csv', names = headers_all)
 
 # converting the data
 df = df.replace('y', 1).replace('n', 0).replace('?', np.nan)
 
+## Challenge 2
 # there are a lot of '?' in the data
 # goal is to replace them with column mean
 df = df.fillna(df.mean())
@@ -20,5 +22,8 @@ df = df.fillna(df.mean())
 # split data into training and test
 from sklearn.cross_validation import train_test_split
 df_train, df_test = train_test_split(df, test_size=0.2, random_state=0)
+
+## Challenge 3
+# Using scikit.learn's KNN algorithm, train a model that predicts the party (republican/democrat)
 
 
