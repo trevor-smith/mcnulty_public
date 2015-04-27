b3052c48b69c5fe54dc0eecc8f77dc317bd5f172
trevor smith <trevor.stuard@gmail.com>
2015-04-27 11:51:47 -0400

    challenges start

---
+++ challenges.py
@@ -0,0 +1,42 @@
+# headers for the data
+headers = ['party', 'handicapped-infants', 'water-project-cost-sharing', 
+'adoption-of-the-budget-resolution', 'physician-fee-freeze', 
+'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban', 
+'aid-to-nicraguan-contras', 'mx-missile', 'immigration', 
+'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue', 
+'crime', 'duty-free-exports', 'export-administration-act-south-africa']
+
+# pickled the csv data...no formatting made
+with open('data.pkl', 'w') as f:
+    pickle.dump(data, f)
+
+with open('data.pkl', 'r') as f:
+    data_unformatted = pickle.load(f)
+
+import pandas as pd
+import numpy as np
+import pickle
+
+import pandas as pd
+import numpy as np
+
+headers_all = ['party', 'handicapped-infants', 'water-project-cost-sharing', 
+'adoption-of-the-budget-resolution', 'physician-fee-freeze', 
+'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban', 
+'aid-to-nicraguan-contras', 'mx-missile', 'immigration', 
+'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue', 
+'crime', 'duty-free-exports', 'export-administration-act-south-africa']
+df = pd.read_csv('data.csv', names = headers_all)
+
+# converting the data
+df = df.replace('y', 1).replace('n', 0).replace('?', np.nan)
+
+# there are a lot of '?' in the data
+# goal is to replace them with column mean
+df = df.fillna(df.mean())
+
+# split data into training and test
+from sklearn.cross_validation import train_test_split
+df_train, df_test = train_test_split(df, test_size=0.2, random_state=0)
+
+
