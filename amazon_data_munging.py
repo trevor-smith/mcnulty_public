91c960d7839366e518d10d02b8821590c253bb33
trevor smith <trevor.stuard@gmail.com>
2015-05-01 16:44:10 -0400

    some data transforms for our data frames

--- amazon_data_munging.py
+++ amazon_data_munging.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib as plt
+import pylab as pl
+%matplotlib inline
+%pylab inline
 
 def parse(path):
   g = open(path, 'r')
@@ -8,6 +12,7 @@ def parse(path):
 obj_reviews = parse('/Users/trevorsmith/Documents/metis/mcnulty_public/reviews_sports_outdoors_sample.txt')
 obj_meta = parse('/Users/trevorsmith/Documents/metis/mcnulty_public/meta_sports_outdoors_sample.txt')
 
+# loop through every review and product and create list of dictionaries
 reviews = []
 for i in obj_reviews:
     reviews.append(i)
@@ -19,5 +24,30 @@ for i in obj_meta:
 df_meta = pd.DataFrame(meta)
 df_reviews = pd.DataFrame(reviews)
 
-print df_meta.head(1)
-print df_reviews.head(1)
+# dropping columns we don't need
+df_meta.drop('categories', axis = 1, inplace = True)
+df_meta.drop('imUrl', axis = 1, inplace = True)
+df_meta.drop('related', axis = 1, inplace = True)
+df_meta.drop('salesRank', axis = 1, inplace = True)
+df_reviews.drop('reviewTime')
+
+df_reviews['unixReviewTime'] = pd.to_datetime(df_reviews['unixReviewTime'],unit='s')
+
+# checking the distribution of review scores
+df_reviews['overall'].hist(bins = 5)
+
+# gotta format the helpful column
+df_reviews['helpful'] = df_reviews['helpful'].astype(str)
+df_reviews['helpful'] = df_reviews['helpful'].str.replace('[','').str.replace(']','')
+df_reviews['helpful'] = pd.DataFrame(df_reviews.helpful.str.split(',').tolist(), columns = "helpful overall".split())
+
+df_reviews['helpful'] = df_reviews.helpful.astype(float)
+df_reviews['overall'] = df_reviews.overall.astype(float)
+
+# let's create another column just for fun :P
+df_reviews['percent helpful'] = df_reviews['helpful'] / df_reviews['overall']
+df_reviews['percent helpful'] = df_reviews['percent helpful'].clip_upper(1)
+
+
+
+
